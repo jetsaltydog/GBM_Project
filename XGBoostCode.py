09921ce40f0662dcf9ff72b45f245b01af3d7c87
henryprosser <44871861+henryprosser@users.noreply.github.com>
2020-02-07 17:26:01 +0000

    Add files via upload

--- XGBoostCode.py
+++ XGBoostCode.py
@@ -43,7 +43,7 @@ test_size = 0.2
 # Initialise a timer
 start_time = time.time()
 
-X_train, X_test, y_train, y_test = train_test_split(array, Y, test_size=test_size, random_state=seed)
+X_train, X_test, y_train, y_test = train_test_split(new_X, Y, test_size=test_size, random_state=seed)
 
 # Create the model
 model = XGBRegressor(objective ='reg:squarederror')
