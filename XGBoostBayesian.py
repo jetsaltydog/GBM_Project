8ca44d9ea250834415d2d1399c0ce4d65b9e8223
henryprosser <44871861+henryprosser@users.noreply.github.com>
2020-02-07 17:27:27 +0000

    Add files via upload

--- XGBoostBayesian.py
+++ XGBoostBayesian.py
@@ -44,7 +44,7 @@ test_size = 0.2
 # Initialise a timer
 start_time = time.time()
 
-X_train, X_test, y_train, y_test = train_test_split(array, Y, test_size=test_size, random_state=seed)
+X_train, X_test, y_train, y_test = train_test_split(new_X, Y, test_size=test_size, random_state=seed)
 
 def objective(space):
     print(space)
